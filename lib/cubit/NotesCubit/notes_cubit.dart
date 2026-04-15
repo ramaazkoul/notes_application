47dd83530f1848b61e0e08c06da8337c41168b00
ramaazkoul <loloazkoul43@gmail.com>
2026-04-15 12:54:56 +0300

    display notes part one

--- lib/cubit/NotesCubit/notes_cubit.dart
+++ lib/cubit/NotesCubit/notes_cubit.dart
@@ -8,15 +8,9 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
-
+  List<NoteModel> notes = [];
   fetchAllNotes() {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes = notesBox.values.toList();
-      emit(NotesSuccess(notes));
-    } catch (e) {
-      emit(NotesFailure(errorMessage: e.toString()));
-      // TODO
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => NotesCubit())],
+      providers: [BlocProvider(create: (context) => NotesCubit()..fetchAllNotes())],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
